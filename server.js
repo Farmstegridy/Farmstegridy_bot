4f6641caf844aad9d25bb6a08a1af48f9f47bb58
dikenson01 <mailletmatthieu@icloud.com>
2026-05-22 21:39:27 +0200

    Add missing log-error endpoint

--- server.js
+++ server.js
@@ -98,6 +98,11 @@ function createServer(port = 8080) {
     app.use(cors());
     app.use(express.json({ limit: '50mb' }));
     app.use(express.urlencoded({ extended: true, limit: '50mb' }));
+
+    app.post('/api/log-error', (req, res) => {
+        console.log("[CLIENT ERROR]", req.body);
+        res.json({ok: true});
+    });
     app.use(fileUpload({
         limits: { fileSize: 500 * 1024 * 1024 },
         useTempFiles: true,
